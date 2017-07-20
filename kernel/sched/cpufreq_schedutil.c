9aaf4bbdf178c068f7e8445180f13520762ea3ab
Chris Redpath <chris.redpath@arm.com>
2017-07-20 16:34:10 +0100

    UPSTREAM: cpufreq: schedutil: irq-work and mutex are only used in slow path
    Execute the irq-work specific initialization/exit code only when the
    fast path isn't available.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 21ef57297b15a49b0c4dd4e7135c1a08e9a29a1c)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Change-Id: Icfd68f455ef71846d799fcd2d8ec6aa1bf59573e
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Tkkg1994 <luca@grifodev.ch>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -561,15 +561,12 @@ static struct sugov_policy *sugov_policy_alloc(struct cpufreq_policy *policy)
 		return NULL;
 
 	sg_policy->policy = policy;
-	init_irq_work(&sg_policy->irq_work, sugov_irq_work);
-	mutex_init(&sg_policy->work_lock);
 	raw_spin_lock_init(&sg_policy->update_lock);
 	return sg_policy;
 }
 
 static void sugov_policy_free(struct sugov_policy *sg_policy)
 {
-	mutex_destroy(&sg_policy->work_lock);
 	kfree(sg_policy);
 }
 
@@ -599,6 +596,9 @@ static int sugov_kthread_create(struct sugov_policy *sg_policy)
 
 	sg_policy->thread = thread;
 	kthread_bind_mask(thread, policy->related_cpus);
+	init_irq_work(&sg_policy->irq_work, sugov_irq_work);
+	mutex_init(&sg_policy->work_lock);
+
 	wake_up_process(thread);
 
 	return 0;
@@ -608,6 +608,7 @@ static void sugov_kthread_stop(struct sugov_policy *sg_policy)
 {
 	flush_kthread_worker(&sg_policy->worker);
 	kthread_stop(sg_policy->thread);
+	mutex_destroy(&sg_policy->work_lock);
 }
 
 static struct sugov_tunables *sugov_tunables_alloc(struct sugov_policy *sg_policy)
