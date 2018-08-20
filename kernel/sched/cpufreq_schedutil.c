c22bf1f0b747fb7e9deec916580f8805f9b99268
sonsation <jhson9304@gmail.com>
2018-08-20 22:30:24 +0900

    kernel/sched/schedutil: remove unused variable
    Signed-off-by: sonsation <jhson9304@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -645,7 +645,6 @@ static int sugov_init(struct cpufreq_policy *policy)
 {
 	struct sugov_policy *sg_policy;
 	struct sugov_tunables *tunables;
-	unsigned int lat;
 	int ret = 0;
 
 	/* State should be equivalent to EXIT */
