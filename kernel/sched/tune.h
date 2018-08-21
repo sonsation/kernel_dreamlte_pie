7b8286655d7fbc318953b259eb6a6e52c0618e18
sonsation <jhson9304@gmail.com>
2018-08-21 18:40:47 +0900

    kernel/sched/tune.h: Delete unnecessary stuffs
    Signed-off-by: sonsation <jhson9304@gmail.com>

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -1,41 +1,6 @@
 
 #ifdef CONFIG_SCHED_TUNE
 
-#ifdef CONFIG_FREQVAR_SCHEDTUNE
-#include <linux/cpufreq.h>
-#include <linux/cpumask.h>
-#include <linux/of.h>
-
-#define SCHEDTUNE_LOAD_BOOST_UTIT 10
-
-struct freqvar_boost_state {
-	struct freqvar_boost_table *table;
-	bool enabled;	/* boost enabled */
-	int ratio;	/* current boost ratio */
-};
-
-struct freqvar_boost_table {
-	int frequency;
-	int boost;
-};
-
-struct freqvar_boost_data {
-	struct freqvar_boost_table *table;
-};
-
-int schedtune_freqvar_boost(int cpu);
-int schedtune_freqvar_boost_init(struct cpufreq_policy *policy, struct freqvar_boost_data *data);
-int schedtune_freqvar_boost_exit(struct cpufreq_policy *policy, struct freqvar_boost_data *data);
-int schedtune_freqvar_update_table(unsigned int *src, int src_size,
-					struct freqvar_boost_table *dst);
-#else
-static inline int schedtune_freqvar_boost(int cpu) { return 0; }
-static inline int schedtune_freqvar_boost_init(struct cpufreq_policy *policy,
-						struct freqvar_boost_data *data) { return 0; };
-static inline int schedtune_freqvar_boost_exit(struct cpufreq_policy *policy,
-						struct freqvar_boost_data *data) { return 0; };
-#endif
-
 #include <linux/reciprocal_div.h>
 
 /*
