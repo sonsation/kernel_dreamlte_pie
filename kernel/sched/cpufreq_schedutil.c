7ef26bf7b1b6a80e6c5bdfecec64baddc7e33017
Son Jong Heon <jhson9304@gmail.com>
2018-08-28 22:57:59 +0900

    kernel/sched/schedutil: Freq always has maximum value
    Signed-off-by: Son Jong Heon <jhson9304@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -187,8 +187,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 				  unsigned long util, unsigned long max)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
-	unsigned int freq = arch_scale_freq_invariant() ?
-				policy->cpuinfo.max_freq : policy->cur;
+	unsigned int freq = policy->cpuinfo.max_freq;
 
 	freq = freqvar_tipping_point(policy->cpu, freq) * util / max;
 
