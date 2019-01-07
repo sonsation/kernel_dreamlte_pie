d8a542124b14f3174e253cb0f90ffb853f7e612e
Diep Quynh <remilia.1505@gmail.com>
2019-01-07 16:01:05 +0000

    cpufreq: Fix Samsung's ifdef derp
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -521,7 +521,7 @@ static void cpufreq_notify_post_transition(struct cpufreq_policy *policy,
 void cpufreq_freq_transition_begin(struct cpufreq_policy *policy,
 		struct cpufreq_freqs *freqs)
 {
-#if defined(CONFIG_SMP) && defined(DEFAULT_USE_ENERGY_AWARE)
+#if defined(CONFIG_SMP) && defined(CONFIG_DEFAULT_USE_ENERGY_AWARE)
 	int cpu;
 #endif
 
@@ -550,7 +550,7 @@ wait:
 	policy->transition_task = current;
 
 	spin_unlock(&policy->transition_lock);
-#ifdef DEFAULT_USE_ENERGY_AWARE
+#ifdef CONFIG_DEFAULT_USE_ENERGY_AWARE
 	scale_freq_capacity(policy->cpus, freqs->new, policy->cpuinfo.max_freq);
 #ifdef CONFIG_SMP
 	for_each_cpu(cpu, policy->cpus)
